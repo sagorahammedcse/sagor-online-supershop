5b27da3aa8245bb7bf507f5cf5b2d1853117e544
sagorahammedcse <sagorahammed164@gmail.com>
2023-10-12 16:24:00 +0600

    added supershop management

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+let countDate = new Date('September 13, 2022 00:00:00').getTime();
+
+function CountDown() {
+  let now = new Date().getTime();
+  gap = countDate - now;
+
+  let second = 1000;
+  let minute = second * 60;
+  let hour = minute * 60;
+  let day = hour * 24;
+
+  let d = Math.floor(gap / day);
+  let h = Math.floor((gap % day) / hour);
+  let m = Math.floor((gap % hour) / minute);
+  let s = Math.floor((gap % minute) / second);
+
+  document.getElementById('day').innerText = d;
+  document.getElementById('hour').innerText = h;
+  document.getElementById('minute').innerText = m;
+  document.getElementById('second').innerText = s;
+}
+
+setInterval(function () {
+  CountDown();
+}, 1000);
+
+function alertCart() {
+  alert('JBHbjhsdjk');
+}
